55ee07ba543154ee9a0801fef446445e7f9ec2ce
terrafrost <terrafrost@php.net>
2013-03-02 19:17:38 -0600

    Add link back to github.com project

--- index.php
+++ index.php
@@ -16,6 +16,8 @@
 </head>
 <body bgcolor="#E5E5E5" text="#000000" link="#006699" vlink="#5493B4">
 
+<a href="https://github.com/terrafrost/sqlparser"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
+
 <a name="top" id="top"></a>
 <table width="100%" cellspacing="0" cellpadding="10" border="0" align="center">
 	<tr>
